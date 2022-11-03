854e3f76cb98d351f9d04bc97e235c6936c20005
Rizky Alfiansyah <rizkkyaf@gmail.com>
2022-11-04 04:20:20 +0800

    change name

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,4 +1,4 @@
-export { default as Headers } from "./Headers";
+export { default as Headers } from "./default-header";
 export { default as MainHeader } from "./main/main-header";
 export { default as Sidebar } from "./main/sidebar";
 export { default as MainLayout } from "./main/main-layout";
